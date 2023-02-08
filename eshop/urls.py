8df2a8c5d867cd5644196fa4973a663d0e9a4bc1
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-02-08 14:31:42 +0300

    create more paths

--- eshop/urls.py
+++ eshop/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
-from .views import ProductListView,CreateStripeCheckoutSessionView
+from .forms import LoginForm
+from .views import ProductListView,CreateStripeCheckoutSessionView,RegisterView,ProfileView
+
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="product-list"),
+    path("", ProductListView.as_view(), name="home"),
+    path('register/', RegisterView.as_view(), name='users-register'),
     path("category/<int:id>",views.product,name="product-detail"),
     path(
         "create-checkout-session/<int:pk>/",
@@ -13,7 +16,9 @@ urlpatterns = [
         name="create-checkout-session",
     ),
     path("cart/",views.cart,name="cart"),
-    
+    path("profile/",ProfileView.as_view(),name="profile"),
+    path("Updateprofile",views.profile_update,name="Updateprofile"),
+    path("login/",views.login_request,name="login")
 ]
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
