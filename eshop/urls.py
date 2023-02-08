a1cb1d12fd93805137552621f29d3d4a9ddb15b5
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-02-08 20:46:16 +0300

    create cart path

--- eshop/urls.py
+++ eshop/urls.py
@@ -3,11 +3,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from .forms import LoginForm
-from .views import ProductListView,CreateStripeCheckoutSessionView,RegisterView,ProfileView
+from .views import CreateStripeCheckoutSessionView,RegisterView,ProfileView
 
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="home"),
+    path("", views.home, name="home"),
     path('register/', RegisterView.as_view(), name='users-register'),
     path("category/<int:id>",views.product,name="product-detail"),
     path(
@@ -16,8 +16,10 @@ urlpatterns = [
         name="create-checkout-session",
     ),
     path("cart/",views.cart,name="cart"),
+    path("update_item/",views.updateItem,name="update_item"),
     path("profile/",ProfileView.as_view(),name="profile"),
     path("Updateprofile",views.profile_update,name="Updateprofile"),
+    path("logout",views.logout_request,name="logout"),
     path("login/",views.login_request,name="login")
 ]
 if settings.DEBUG:
