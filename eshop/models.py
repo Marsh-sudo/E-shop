30abdf6c8b25a14c782fecf81e74541a618ee7fb
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-02-08 14:32:55 +0300

    create properties

--- eshop/models.py
+++ eshop/models.py
@@ -106,3 +106,20 @@ class OrderItem(models.Model):
 	def get_total(self):
 		total = self.product.price * self.quantity
 		return total
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+        if img.height > 100 or img.width > 100:
+            new_img = (20, 20)
+            img.thumbnail(new_img)
+            img.save(self.avatar.path)
