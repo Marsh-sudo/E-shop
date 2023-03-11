1584ce39be292b5a371632b327105814d47642ce
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-03-11 15:50:17 +0300

    create properties

--- eshop/models.py
+++ eshop/models.py
@@ -113,7 +113,8 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    
+    def __str__(self):
+        return str(self.product)
 
     
     @property
