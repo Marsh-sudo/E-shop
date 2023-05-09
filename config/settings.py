8e42fc8308b5377be36800b3fc1e6bcfa47a7640
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-05-10 01:11:11 +0300

    configure database

--- config/settings.py
+++ config/settings.py
@@ -88,9 +88,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'eshop',
-        'USER': 'biggie',
-        'PASSWORD': '123456',
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config('DB_HOSTNAME'),
+        'PORT': config('DB_PORT', cast=int),
     }
 }
 
