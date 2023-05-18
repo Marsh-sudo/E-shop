6e9afb3fa83a923ee3bf71d3017111b72e88fcf4
Munyao Kelvin <biggie@users-MacBook-Pro.local>
2023-05-18 16:53:11 +0300

    change something in setting

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import csv
 from pathlib import Path
 import os
 from decouple import config
@@ -35,7 +36,6 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
